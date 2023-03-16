53e1b7fe20b2db0a133d3f995aeb32a200660bec
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-03-16 11:03:29 -0600

    cerradura de afn y afn a afd

---
+++ AFD.py
@@ -0,0 +1,39 @@
+import graphviz
+
+class AFD():
+
+    def __init__(self, estado_inicial, alfabeto = [], estados = [], estados_de_aceptacion = [], transitions = {}, estado_final = None) -> None:
+        
+        self.estado_final = estado_final
+        self.alfabeto: list = alfabeto #Aceptado como trancisiones (nombre de arista, digamos)
+        self.estados: list = estados #Estados (vertices)
+        self.estado_inicial = estado_inicial
+        self.estados_de_aceptacion: list = [item for item in estados_de_aceptacion if item in self.estados]
+        self.transitions: dict = transitions 
+
+
+    def draw_afd(self):
+        afn = graphviz.Digraph(format='pdf')
+        afn.graph_attr['rankdir'] = 'LR'
+        afn.node('start', style='invis')
+        nodos = []
+        for state in self.estados:
+            if state==self.estado_inicial:
+                afn.node(state, shape='diamond', color='red')
+                nodos.append((state, "inicial"))
+            elif state not in self.estados_de_aceptacion:
+                afn.node(state)
+                nodos.append((state, "normal"))
+            else:
+                afn.node(state, shape='doublecircle')
+                nodos.append((state, "aceptacion"))
+
+        grafo = []
+        for state in self.transitions:
+            trans = self.transitions.get(state)
+            for symbol in trans:
+                if symbol !='ε':
+                    next_state = trans.get(symbol)
+                    afn.edge(state, next_state, label=symbol)
+
+        afn.render('afd', view=True)
--- AFN.py
+++ AFN.py
@@ -1,14 +1,16 @@
 import graphviz
+from stack import Stack
+from AFD import AFD
+from collections import deque
 
 class AFN():
 
     def __init__(self, estado_inicial, alfabeto = [], estados = [], estados_de_aceptacion = [], transitions = {}, estado_final = None) -> None:
-        
         self.estado_final = estado_final
         self.alfabeto: list = alfabeto #Aceptado como trancisiones (nombre de arista, digamos)
         self.estados: list = estados #Estados (vertices)
         self.estado_inicial = estado_inicial
-        self.estados_de_aceptacion: list = [item for item in estados_de_aceptacion if item in self.estados]
+        self.estados_de_aceptacion: set = set([item for item in estados_de_aceptacion if item in self.estados])
 
         self.transitions: dict = transitions if transitions else {estado: {caracter: [] for caracter in self.alfabeto} for estado in self.estados}#Trancisiones que se realizan entre (representa las aristas), Vertical:  // None indica que no hay trancision
         self.hasTransitionE: bool = 'ε' in self.alfabeto 
@@ -37,3 +39,66 @@ class AFN():
                         afn.edge(state, next_state, label=symbol)
 
         afn.render('afn', view=True)
+    
+    def find_cerradura(self):
+        cerradura = {state: set() for state in self.estados}
+        for state in self.estados:
+            closure = set()
+            stack = Stack()
+            stack.push(state)
+            while stack.items:
+                current_state = stack.pop()
+                closure.add(current_state)
+                epsilon_transitions = self.transitions[current_state].get('ε', [])
+                for next_state in epsilon_transitions:
+                    if next_state not in closure:
+                        stack.push(next_state)
+            cerradura[state] = closure
+        self.cerraduras_de_estados = cerradura
+        return cerradura
+
+    def to_afd(self):
+        tabla_trancisiones_AFD = {'VACIO': {input: 'VACIO' for input in self.alfabeto}}# Tabla inicialmente con el estado vacío
+        new_states  = ['VACIO']
+        accepted_states = []
+        needs_empty_state = False
+        
+        def esEstadoAceptado(nuevoEstado: set):
+            index = 0
+            aceptado = False
+            nuevoEstado = list(nuevoEstado)
+            while index!=len(nuevoEstado) and not aceptado:
+                if nuevoEstado[index] in self.estados_de_aceptacion: aceptado = True
+                index += 1        
+            return aceptado
+        
+        pendientes = [self.cerraduras_de_estados.get(self.estado_inicial)]
+
+        while len(pendientes)!=0:
+            estado_tratado = pendientes[0]
+            new_states.append(estado_tratado)
+            transitions = {}
+            for input in self.alfabeto:
+                if input!= 'ε':
+                    next_state = set()
+                    for estado_de_cerradura in estado_tratado:
+                        next_possible_states = self.transitions.get(estado_de_cerradura).get(input)
+                        if next_possible_states:
+                            for estados_siguientes in next_possible_states:
+                                for cerr in self.cerraduras_de_estados.get(estados_siguientes): 
+                                    next_state.add(cerr)
+                            
+                    if next_state:
+                        transitions[input] = str(next_state)
+                        if not next_state in new_states: 
+                            pendientes.append(next_state)
+                            if esEstadoAceptado(next_state) and not str(next_state) in accepted_states: accepted_states.append(str(next_state))
+                    else:
+                        needs_empty_state = True
+                        transitions[input] = 'VACIO'
+            tabla_trancisiones_AFD[str(estado_tratado)] = transitions
+            pendientes.pop(0)
+        if not needs_empty_state:
+            new_states.pop(0)
+            tabla_trancisiones_AFD.pop('VACIO')
+        return AFD(str(self.cerraduras_de_estados.get(self.estado_inicial)), [alf for alf in self.alfabeto if alf != 'ε'], [str(state) for state in new_states], accepted_states, tabla_trancisiones_AFD)
